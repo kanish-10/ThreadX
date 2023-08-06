4afc595db1b0bbe336fc9514242c46138f750d9e
Kanish Chheda <kanishchheda@gmail.com>
2023-08-07 00:57:33 +0530

    Home page and Layout done

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div className="text-white">Onboarding</div>;
+};
+
+export default page;
---
+++ components/shared/index.ts
@@ -0,0 +1,6 @@
+import TopBar from "./TopBar";
+import BottomBar from "./BottomBar";
+import LeftSideBar from "./LeftSideBar";
+import RightSideBar from "./RightSideBar";
+
+export { TopBar, BottomBar, LeftSideBar, RightSideBar };
