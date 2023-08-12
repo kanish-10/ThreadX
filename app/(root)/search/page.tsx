f7ab5d77bbb92aadf5aad133fabc0e0e6047fd5c
Kanish Chheda <kanishchheda@gmail.com>
2023-08-13 00:31:33 +0530

    Search completed

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,52 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  //Fetch User
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No Users</p>
+        ) : (
+          <>
+            {result.users.map((profile) => (
+              <UserCard
+                key={profile.id}
+                id={profile.id}
+                name={profile.name}
+                username={profile.username}
+                imgUrl={profile.image}
+                profileType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default Page;
